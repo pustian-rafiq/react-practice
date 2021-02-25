680b9578c57a0c9b651a2d5e52fe616d5a28bff8
Rafiqul_Pust <rafiqul.pust.cse@gmail.com>
2021-02-25 12:12:15 +0600

    nested state

--- src/components/StateComponent.js
+++ src/components/StateComponent.js
@@ -6,20 +6,30 @@ class StateComponent extends Component{
   constructor(){
       super()
       this.state={
-          name : "Rafiqul Islam",
-          district  : "Satkhira",
-          fathername : "Moslem Ali Boddi",
-          mothername    : "Alyea Begum"
+          name : ["Rafiqul Islam","Moslem Ali","Aleya Khatun"],
+          age   : 24,
+          information:{
+              village   : "Brozopatulia",
+              post      : "Bagbati",
+              thana     : "Kaligonj",
+              district  : "Satkhira"
+          }
       }
   }
 
     render(){
         return <div className="container">
-
-            <p className="bg-dark text-center text-danger p-5">My Name is {this.state.name}</p>
-            <p className="bg-dark text-center text-danger p-5">My District is {this.state.district}</p>
-            <p className="bg-dark text-center text-danger p-5">My Father Name is {this.state.fathername}</p>
-            <p className="bg-dark text-center text-danger p-5">My Mother Name is {this.state.mothername}</p>
+    <p className="bg-primary">My personal Information</p>
+            <span className=" text-center text-danger p-5">My Name is {this.state.name[0]}</span>
+            <span className="  text-center text-danger p-5">My Age is {this.state.age}</span>
+            <span className="  text-center text-danger p-5">My Father Name is {this.state.name[1]}</span>
+            <span className="  text-center text-danger p-5">My Mother Name is {this.state.name[2]}</span>
+            <br></br>
+         <h1 className="bg-success">Permanent Address</h1>
+            <span className="  text-center text-danger p-5">My District is {this.state.information.village}</span>
+            <span className=" text-center text-danger p-5">My District is {this.state.information.post}</span>
+            <span className=" text-center text-danger p-5">My District is {this.state.information.thana}</span>
+            <span className="  text-center text-danger p-5">My District is {this.state.information.district}</span>
         </div>
     }
 }
