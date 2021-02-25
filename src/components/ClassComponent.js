ce99f8099b10803667f61b6e2bd37a7504d9947b
Rafiqul_Pust <rafiqul.pust.cse@gmail.com>
2021-02-25 07:40:26 +0600

    functional and class component

---
+++ src/components/ClassComponent.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react'
+
+class ClassComponent extends Component{
+    render(){
+      return  <h1>Hello, I am from Class Component.</h1>
+    }
+
+}
+export default ClassComponent
---
+++ src/components/FunctionalComponent.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+function FunctionalComponent(){
+    return(
+        <h1>Hello, I am from Functional Component.</h1>
+    );
+}
+
+export default FunctionalComponent
