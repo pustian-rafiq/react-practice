f327fe665f0551795cfa7a21781d77e470e5cf19
Rafiqul_Pust <rafiqul.pust.cse@gmail.com>
2021-02-25 07:55:59 +0600

    props

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@
 import './App.css';
 import FunctionalComponent from './components/FunctionalComponent'
 import ClassComponent from './components/ClassComponent'
+import Props from './components/Props'
 
 function App() {
   return (
     <div className="App">
        <FunctionalComponent></FunctionalComponent>
        <ClassComponent></ClassComponent>
+       <Props name="Rafiqul" age="24"></Props>
+       <Props name="Ruma" age="20"></Props>
+       <Props name="Rabiul" age="29"></Props>
+       <Props name="Kamrunnahar" age="21"></Props>
     </div>
   );
 }
---
+++ src/components/Props.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+// Props is used for assigning and Reuse of Component With Custom Value
+
+function Props(props){
+    return(
+        <h1>Hello, I am {props.name} and my age is {props.age}</h1>
+    );
+}
+
+export default Props
